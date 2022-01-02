3b3d23f8378dba4b56c17043a17c1f8075c86fe2
dirma <syys0104@gmail.com>
2022-01-02 19:37:23 +0900

    루트시그니처, 메쉬, 쉐이더 추가

--- CatchBear/Engine/Engine.h
+++ CatchBear/Engine/Engine.h
@@ -3,6 +3,9 @@
 #include "Device.h"
 #include "CommandQueue.h"
 #include "SwapChain.h"
+#include "RootSignature.h"
+#include "Mesh.h"
+#include "Shader.h"
 
 class Engine
 {
@@ -14,6 +17,7 @@ public:
 	shared_ptr<Device> GetDevice() { return _device; }
 	shared_ptr<CommandQueue> GetCmdQueue() { return _cmdQueue; }
 	shared_ptr<SwapChain> GetSwapChain() { return _swapChain; }
+	shared_ptr<RootSignature> GetRootSignature() { return _rootSignature; }
 
 public:
 	void RenderBegin();
@@ -30,6 +34,7 @@ private:
 	shared_ptr<Device>			_device;
 	shared_ptr<CommandQueue>	_cmdQueue;
 	shared_ptr<SwapChain>		_swapChain;
+	shared_ptr<RootSignature>	_rootSignature;
 	
 };
 

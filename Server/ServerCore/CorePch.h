8f4bb25df9db5d616f8bd87d94159fd6511e25c5
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-01-03 19:52:35 +0900

    서버 환경설정

---
+++ Server/ServerCore/CorePch.h
@@ -0,0 +1,17 @@
+#pragma once
+// 게임서버나 더미클라이언트에서도 참조하기 원하는 미리참조된헤더
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();

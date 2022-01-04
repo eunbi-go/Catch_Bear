00d0096684a79326dffd0f09eb3d0f0d34baf278
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-01-05 01:01:54 +0900

    Condition Variable (event에 조건을 걸어주는 동기화기법)

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -10,18 +10,26 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// 참고) CV는 User-Level Object (커널 오브젝트X)
+condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
+		cv.notify_one();	// wait중인 쓰레드가 있으면 딱 1개를 깨운다
 
-		this_thread::sleep_for(100ms);
+		//this_thread::sleep_for(100ms);
 	}
 }
 
@@ -29,15 +37,21 @@ void Consumor()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
-		//::ResetEvent(handle);		매뉴얼이 아닐때(두 번째인자 true)
-
 		unique_lock<mutex> lock(m);
-		if (q.empty() == false)
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡고
+		// 2) 조건 확인
+		// - 만족O => 빠져 나와서 이어서 코드를 진행
+		// - 만족X => Lock을 풀어주고 대기 상태
+
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		// notify_one 할때 lock을 잡고 있는 것이 아니기 때문
+
+		//if (q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }

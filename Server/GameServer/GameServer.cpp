431411e4a43db0d3af7bbd8ed4bbca974b6e3f2d
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-01-05 01:38:32 +0900

    future 코드

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,72 +4,86 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include <Windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-// 참고) CV는 User-Level Object (커널 오브젝트X)
-condition_variable cv;
-
-void Producer()
+int64 Calculate()
 {
-	while (true)
-	{
-		// 1) Lock을 잡고
-		// 2) 공유 변수 값을 수정
-		// 3) Lock을 풀고
-		// 4) 조건변수 통해 다른 쓰레드에게 통지
+	int64 sum = 0;
+	
+	for (int32 i = 0; i < 100'000; i++)
+		sum += i;
 
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
-
-		cv.notify_one();	// wait중인 쓰레드가 있으면 딱 1개를 깨운다
-
-		//this_thread::sleep_for(100ms);
-	}
+	return sum;
 }
 
-void Consumor()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true)
-	{
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() {return q.empty() == false; });
-		// 1) Lock을 잡고
-		// 2) 조건 확인
-		// - 만족O => 빠져 나와서 이어서 코드를 진행
-		// - 만족X => Lock을 풀어주고 대기 상태
+	promise.set_value("msg");
+}
 
-		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
-		// notify_one 할때 lock을 잡고 있는 것이 아니기 때문
-
-		//if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << q.size() << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal / Non-Signal << bool
-	// Auto / Manual << bool
-	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	// 동기 실행
+	//int64 sum = Calculate();
+	//cout << sum << endl;
+	
+	// std::future
+	{
+		// 1) deferred -> 지연해서 실행하세요 (Calculate를 나중에 호출하는거랑 별반 다를게 없다) -> 요청하는 시점과 호출하는 시점이 다르므로 비동기
+		// 2) async -> 별도의 쓰레드를 만들어서 실행하세요
+		// 3) deferred | async -> 둘 중 알아서 골라주세요
 
-	thread t1(Producer);
-	thread t2(Consumor);
+		// 언젠가 미래에 결과물을 뱉어줄거야!
+		std::future<int64> future = std::async(std::launch::async, Calculate);
+		
+		// TODO
 
-	t1.join();
-	t2.join();
 
-	::CloseHandle(handle);
+		// 결과물이 궁금할때 비로소 이걸 호출해서 확인함
+		int64 sum = future.get();
+	}
+
+	// std::promise
+	{
+		// 미래(std::future)에 결과물을 반환해줄거라 약속(std::promise) (계약서?)
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, std::move(promise));
+
+		string msg = future.get();
+		cout << msg << endl;
+
+		t.join();
+	}
+
+	// std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << endl;
+
+		t.join();
+	}
+
+	// 결론)
+	// mutex, condition_variable까지 가지 않고 단순한 애들을 처리하기 좋다
+	// 특히나 한번 발생하는 이벤트에 유용하다
+	// 닭잡는데 소잡는 칼 쓸 필요 없다
+	// 1) async
+	// 원하는 함수를 비동기적으로 실행
+	// 2) promise
+	// 결과물을 promise를 통해 future로 받아줌
+	// 3) package_task
+	// 원하는 함수의 실행 결과를 package_task를 통해 future로 받아줌 
 }
 

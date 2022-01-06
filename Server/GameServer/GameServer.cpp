ee58b37fd080760dbfdd974e57ad782c4a5be5d5
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-01-07 02:02:05 +0900

    Thread Local Storage

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,40 +6,31 @@
 #include <mutex>
 #include <future>
 
-//atomic<bool> flag;
+//__declspec(thread) int32 value;
+thread_local int32 LThreadId = 0;
+
+void ThreadMain(int32 threadId)
+{
+	LThreadId = threadId;
+
+	while (true)
+	{
+		cout << "Hi i am Thread" << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 int main()
 {
-	//flag = false;
+	vector<thread> threads;
 
-	//// flag = true;
-	//flag.store(true, memory_order::memory_order_seq_cst);
+	for (int32 i = 0; i < 10; ++i)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
 
-	//// bool val = flag;
-	//bool val = flag.load(memory_order::memory_order_seq_cst);
-
-	//// 이전 flag값을 prev에 넣고, flag값을 수정
-	//{
-	//	bool prev = flag.exchange(true);
-	//	// 밑에 이거처럼 하면 원자적이지 않으므로 다른 스레드에서 공용 데이터를 잘못 건드릴수있음
-	//	//bool prev = flag;
-	//	//flag = true;
-	//}
-
-	//// CAS 조건부 수정
-	//{
-	//	bool expected = false;
-	//	bool desired = true;
-	//	flag.compare_exchange_strong(expected, desired);
-	//}
-
-	// Memory Model (정책)
-	// 1) Sequentially Consistent (seq_cst)
-	// 2) Acquire-Release (acquire, release)
-	// 3) Relaxed (relaxed)
-
-	// 1) sez_cst (가장 엄격 = 컴파일러 최적화 여지 적음 = 직관적)
-	// 2) acquire-release
-	// 3) relaxed (자유롭다 = 컴파일러 최적화 여지 많음 = 직관적이지 않음)
+	for (thread& t : threads)
+		t.join();
 }
 

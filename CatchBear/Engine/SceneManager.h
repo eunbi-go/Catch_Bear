0e2fd6c2b01321c7b8d822252fe4c07c66dae551
dirma <syys0104@gmail.com>
2022-01-12 18:34:05 +0900

    Scene, SceneManager 추가

---
+++ CatchBear/Engine/SceneManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// 싱글톤
+// 언제 어디서나 활용할 수 있도록
+// 유일성 보장
+
+class Scene;
+
+class SceneManager
+{
+	DECLARE_SINGLE(SceneManager);
+
+public:
+	void Update();
+	void LoadScene(wstring sceneName);
+
+public:
+	shared_ptr<Scene> GetActiveScene() { return _activeScene; }
+
+private:
+	shared_ptr<Scene> LoadTestScene();
+
+private:
+	shared_ptr<Scene> _activeScene;
+};
+

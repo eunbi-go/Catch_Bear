a0a81af0e338a489e903ecb1bfe32603c9f7aedd
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-04-09 03:46:23 +0900

    병합용 커밋

---
+++ CatchBear/Engine/TagMark.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "TagMark.h"
+#include "Timer.h"
+#include "Transform.h"
+#include "GameObject.h"
+#include "Component.h"
+#include "SceneManager.h"
+#include "Scene.h"
+
+TagMark::TagMark()
+{
+}
+
+TagMark::~TagMark()
+{
+}
+
+void TagMark::Update()
+{
+	Vec3 rotation = GetTransform()->GetLocalRotation();
+	rotation.y += DELTA_TIME * _fRotationSpeed;
+	GetTransform()->SetLocalRotation(rotation);
+
+	_target = GET_SINGLE(SceneManager)->GetActiveScene()->GetGameObject(L"Player");
+	Vec3 transform = _target->GetTransform()->GetLocalPosition();
+	transform.y += 2.f;
+	GetTransform()->SetLocalPosition(transform);
+}
+
+void TagMark::LateUpdate()
+{
+	// 술래되면 색 변하게 해야 함
+	// -> 플-플 충돌 후, 서버에서 알려줘야 함
+	// -> 그 때 구현할게요
+}

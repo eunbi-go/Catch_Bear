816090f777da4a1f1d652d3cab772116d31e92a9
eunbi <geunbi38@gmail.com>
2022-07-23 17:15:56 +0900

    다시시작 시 UI 재배치

--- CatchBear/Engine/SceneManager.cpp
+++ CatchBear/Engine/SceneManager.cpp
@@ -146,6 +146,9 @@ void SceneManager::ReStart()
 
 	// 아이템
 	GET_SINGLE(ItemManager)->Reset();
+
+	// 점수
+	GET_SINGLE(ScoreManager)->InitScore();
 }
 
 
@@ -320,34 +323,34 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 			//_playerTypes[0] = PLAYER_TYPE::PLAYER_TYPE_CNT;
 		}
 
-		//g_EnterPlayerCnt = 2;
-		//CheckPlayerType(1, key);
-		//shared_ptr<CharacterData> CharacData2 = GET_SINGLE(Resources)->LoadCharacter(key);
-		//vector<shared_ptr<GameObject>>	gameObjects2 = CharacData2->Instantiate();
+		g_EnterPlayerCnt = 2;
+		CheckPlayerType(1, key);
+		shared_ptr<CharacterData> CharacData2 = GET_SINGLE(Resources)->LoadCharacter(key);
+		vector<shared_ptr<GameObject>>	gameObjects2 = CharacData2->Instantiate();
 
-		//for (auto& gameObject : gameObjects2)
-		//{
-		//	gameObject->SetName(L"Player2");
-		//	gameObject->GetTransform()->SetLocalPosition(Vec3(10.f, 0.f, 5.f));
-		//	gameObject->GetTransform()->SetLocalScale(Vec3(1.f, 1.f, 1.f));
-		//	gameObject->AddComponent(make_shared<Player>());
-		//	gameObject->GetAnimationController()->SetTrackAnimationSet(0, 0);
-		//	gameObject->GetMeshRenderer()->GetMaterial()->SetInt(0, 0);
-		//	gameObject->SetStatic(false);
-		//	gameObject->SetBoundingExtents(XMFLOAT3(0.4f, 1.f, 0.4f));
-		//	gameObject->SetBoundingBox(BoundingOrientedBox(
-		//		XMFLOAT3(0.0f, 0.0f, 0.0f), gameObject->GetBoundingExtents(), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f)));
-		//	gameObject->SetCheckFrustum(false);
-		//	gameObject->SetPlayerID(1);
-		//	gameObject->_state = new IdleState();
-		//	static_pointer_cast<Player>(gameObject->GetScript(0))->SetTextureKey(key);
-		//	scene->AddGameObject(gameObject);
-		//	scene->AddPlayers(1, gameObject);
-		//	scene->AddVecPlayers(gameObject);
-		//	_isPlayersEnterLobby[0] = false;
-		//	_isPlayersReady[0] = false;
-		//	//_playerTypes[0] = PLAYER_TYPE::PLAYER_TYPE_CNT;
-		//}
+		for (auto& gameObject : gameObjects2)
+		{
+			gameObject->SetName(L"Player2");
+			gameObject->GetTransform()->SetLocalPosition(Vec3(10.f, 0.f, 5.f));
+			gameObject->GetTransform()->SetLocalScale(Vec3(1.f, 1.f, 1.f));
+			gameObject->AddComponent(make_shared<Player>());
+			gameObject->GetAnimationController()->SetTrackAnimationSet(0, 0);
+			gameObject->GetMeshRenderer()->GetMaterial()->SetInt(0, 0);
+			gameObject->SetStatic(false);
+			gameObject->SetBoundingExtents(XMFLOAT3(0.4f, 1.f, 0.4f));
+			gameObject->SetBoundingBox(BoundingOrientedBox(
+				XMFLOAT3(0.0f, 0.0f, 0.0f), gameObject->GetBoundingExtents(), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f)));
+			gameObject->SetCheckFrustum(false);
+			gameObject->SetPlayerID(1);
+			gameObject->_state = new IdleState();
+			static_pointer_cast<Player>(gameObject->GetScript(0))->SetTextureKey(key);
+			scene->AddGameObject(gameObject);
+			scene->AddPlayers(1, gameObject);
+			scene->AddVecPlayers(gameObject);
+			_isPlayersEnterLobby[0] = false;
+			_isPlayersReady[0] = false;
+			//_playerTypes[0] = PLAYER_TYPE::PLAYER_TYPE_CNT;
+		}
 
 		//g_EnterPlayerCnt = 3;		// 최종적으로 3인게임으로 바꾸면 3으로 고정 
 		//CheckPlayerType(2, key);
@@ -426,7 +429,7 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		scene->AddGameObject(gameObject);
 		scene->AddTagMarks(0, gameObject);
 	}
-	/*vector<shared_ptr<GameObject>>	objectsHeart2 = meshHeart->Instantiate();
+	vector<shared_ptr<GameObject>>	objectsHeart2 = meshHeart->Instantiate();
 	for (auto& gameObject : objectsHeart2)
 	{
 		gameObject->SetName(L"PlayerTag2");
@@ -441,7 +444,7 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		scene->AddTagMarks(1, gameObject);
 	}
 
-	vector<shared_ptr<GameObject>>	objectsHeart3 = meshHeart->Instantiate();
+	/*vector<shared_ptr<GameObject>>	objectsHeart3 = meshHeart->Instantiate();
 	for (auto& gameObject : objectsHeart3)
 	{
 		gameObject->SetName(L"PlayerTag3");
